6512935dcfd388fbc50da286e584c8470d153af9
hireoochen@gmail.com <hireoochen@gmail.com>
2021-11-03 14:19:19 +0800

    modiy baseURL

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'https://api-dance.hireoo.fun';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 ReactDOM.render((
